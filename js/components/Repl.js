a8c69768ff3a5cb623d91fcdc5417ecc67e0a881
Benjamin Hetherington <b.w.hetherington@gmail.com>
2019-05-10 01:53:47 -0400

    Update formatting

--- js/components/Repl.js
+++ js/components/Repl.js
@@ -1,23 +1,27 @@
-import React from "react";
-import RLisp from "../../crate/Cargo.toml";
-import { runRLisp } from "../import";
-import "./Repl.css";
+import React from 'react';
+import RLisp from '../../crate/Cargo.toml';
+import { runRLisp } from '../import';
+import './Repl.css';
 
 function renderValue(value, id) {
   if (value !== undefined) {
     switch (value.type) {
-      case "value":
-        return <div key={id}>{value.value}</div>;
-      case "command":
+      case 'value':
+        return (
+          <div key={id}>
+            {'<'} {value.value}
+          </div>
+        );
+      case 'command':
         return (
           <div key={id} className="command">
             {value.value}
           </div>
         );
-      case "exception":
+      case 'exception':
         return (
           <div key={id} className="exception">
-            {value.value}
+            {'*'} {value.value}
           </div>
         );
     }
@@ -34,7 +38,7 @@ class Repl extends React.Component {
       history: [],
       historyIndex: -1,
       messages: [],
-      input: ""
+      input: ''
     };
 
     this.onChangeInput = event => {
@@ -53,12 +57,12 @@ class Repl extends React.Component {
       const output = runRLisp(input);
       const cmd = {
         success: true,
-        type: "command",
+        type: 'command',
         value: `> ${input}`
       };
 
       let messages;
-      if (output.value !== "()") {
+      if (output.value !== '()') {
         messages = [...this.state.messages, cmd, output];
       } else {
         messages = [...this.state.messages, cmd];
@@ -68,7 +72,7 @@ class Repl extends React.Component {
         history,
         historyIndex: history.length - 1,
         messages,
-        input: ""
+        input: ''
       });
     };
   }
